da268b19e98e8ae9089d20cfa1b633a19dc9bd58
100DaysOfCodeChallenge <94281803+BhavKush@users.noreply.github.com>
2023-01-24 01:31:33 +0530

    Create Solution.java

---
+++ Day61 Solve me First (CP)/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+       int a,b,s;
+       Scanner sc = new Scanner(System.in);
+       a=sc.nextInt();
+       b=sc.nextInt();
+       s=a+b;
+       System.out.println(s);
+    }
+}
